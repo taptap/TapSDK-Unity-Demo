0c6f1071f293d94a1adf679ec2b581e3140f0818
yuwenjian <740225978@qq.com>
2022-11-22 19:49:29 +0800

    修改动态、数据分析、防沉迷 UI界面

--- Assets/Scenes/MomentScene.cs
+++ Assets/Scenes/MomentScene.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using TapTap.Moment;
 using UnityNative.Toasts.Example;
+using System;
+
 
 public class MomentScene : MonoBehaviour
 {
-    // Start is called before the first frame update
+        public GUISkin demoSkin;
+
     void Start()
     {
         TapMoment.FetchNotification();
@@ -32,24 +35,40 @@ public class MomentScene : MonoBehaviour
 
     private void OnGUI()
     {
-        GUIStyle style = new GUIStyle(GUI.skin.button);
-        style.fontSize = 40;
 
-        GUI.depth = 0;
+        GUI.skin = demoSkin;
+        float scale = 1.0f;
 
-        if (GUI.Button(new Rect(60, 150, 180, 100), "打开动态", style)) {
+		
+		float btnWidth= Screen.width / 5 * 2;
+        float btnWidth2 = btnWidth + 80 * scale;
+
+        float btnHeight = Screen.height / 25;
+		float btnTop = 30 * scale;
+		float btnGap = 20 * scale;
+
+		GUI.skin.button.fontSize = Convert.ToInt32(13 * scale);
+
+        var style = new GUIStyle(GUI.skin.button) { fontSize = 20 };
+        var labelStyle = new GUIStyle(GUI.skin.label) { fontSize = 30 };
+
+        if (GUI.Button(new Rect((Screen.width - btnGap) / 2 - btnWidth, btnTop, btnWidth /2, btnHeight), "返回", style))
+		{
+            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(0);
+        }
+
+        btnTop += btnHeight + 20 * scale;
+
+		if (GUI.Button(new Rect((Screen.width - btnGap) / 2 - btnWidth, btnTop, btnWidth, btnHeight), "打开动态", style))
+		{
            TapMoment.Open(Orientation.ORIENTATION_DEFAULT);
         }
 
-        if (GUI.Button(new Rect(60, 300, 180, 100), "动态红点", style)) {
-           TapMoment.FetchNotification();
+        if (GUI.Button(new Rect((Screen.width - btnGap) / 2 + btnGap, btnTop, btnWidth, btnHeight), "动态红点", style))
+		{
+            TapMoment.FetchNotification();
         }
 
-        
-
-
-        if (GUI.Button(new Rect(60, 450, 180, 100), "返回", style)) {
-            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(0);
-        }
+    
     }
 }
