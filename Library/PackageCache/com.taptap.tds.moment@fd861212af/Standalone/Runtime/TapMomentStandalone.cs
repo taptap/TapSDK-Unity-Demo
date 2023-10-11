2797729cdf9b80ae75a4a116fe2f38379554c9c5
yuwenjian <740225978@qq.com>
2023-10-11 14:18:10 +0800

    更新 SDK 版本并添加悬浮窗功能

---
+++ Library/PackageCache/com.taptap.tds.moment@fd861212af/Standalone/Runtime/TapMomentStandalone.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using TapTap.Common;
+using TapTap.Moment.Internal;
+
+namespace TapTap.Moment.Standalone {
+    public class TapMomentStandalone : ITapMomentBridge {
+        public void Open(Orientation orientation) {
+            // 打开论坛 web
+            string url;
+            TapConfig config = TapCommon.Config;
+            if (config.RegionType == RegionType.CN) {
+                url = $"https://www.taptap.cn/app/{TapMomentConfig.AppId}/topic?utm_medium=link&utm_source=pc_sdk";
+            } else {
+                url = $"https://www.taptap.io/app/{TapMomentConfig.AppId}?utm_medium=link&utm_source=pc_sdk";
+            }
+            Application.OpenURL(url);
+        }
+
+        public void SetDefaultOrientation(Orientation orientation){
+            TapLogger.Warn($"{nameof(SetDefaultOrientation)} NOT implemented.");
+        }
+        
+        public void Open(){
+            Open(Orientation.ORIENTATION_DEFAULT);
+        }
+
+        public void Close() {
+            TapLogger.Warn($"{nameof(Close)} NOT implemented.");
+        }
+
+        public void Close(string title, string content) {
+            TapLogger.Warn($"{nameof(Close)} NOT implemented.");
+        }
+
+        public void DirectlyOpen(Orientation orientation, string page, Dictionary<string, object> extras) {
+            TapLogger.Warn($"{nameof(DirectlyOpen)} NOT implemented.");
+        }
+
+        public void DirectlyOpen(string page, Dictionary<string, object> extras) {
+            TapLogger.Warn($"{nameof(DirectlyOpen)} NOT implemented.");
+        }
+
+        public void FetchNotification() {
+            TapLogger.Warn($"{nameof(FetchNotification)} NOT implemented.");
+        }
+
+        public void Init(string clientId) {
+            TapLogger.Warn($"{nameof(Init)} NOT implemented.");
+        }
+
+        public void Init(string clientId, bool isCN) {
+        }
+
+        public void NeedDeferSystemGestures() {
+            TapLogger.Warn($"{nameof(NeedDeferSystemGestures)} NOT implemented.");
+        }
+
+        public void Publish(Orientation orientation, string[] imagePaths, string content) {
+            TapLogger.Warn($"{nameof(Publish)} NOT implemented.");
+        }
+
+        public void Publish(string[] imagePaths, string content) {
+            TapLogger.Warn($"{nameof(Publish)} NOT implemented.");
+        }
+
+        public void PublishVideo(Orientation orientation, string[] videoPaths, string[] imagePaths, string title, string desc) {
+            TapLogger.Warn($"{nameof(PublishVideo)} NOT implemented.");
+        }
+
+        public void PublishVideo(string[] videoPaths, string[] imagePaths, string title, string desc) {
+            TapLogger.Warn($"{nameof(PublishVideo)} NOT implemented.");
+        }
+
+        public void PublishVideo(Orientation orientation, string[] videoPaths, string title, string desc) {
+            TapLogger.Warn($"{nameof(PublishVideo)} NOT implemented.");
+        }
+
+        public void PublishVideo(string[] videoPaths, string title, string desc) {
+            TapLogger.Warn($"{nameof(PublishVideo)} NOT implemented.");
+        }
+
+        public void SetCallback(Action<int, string> callback) {
+            TapLogger.Warn($"{nameof(SetCallback)} NOT implemented.");
+        }
+
+        public void SetUseAutoRotate(bool useAuto) {
+            TapLogger.Warn($"{nameof(SetUseAutoRotate)} NOT implemented.");
+        }
+    }
+}
